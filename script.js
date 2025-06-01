b20e3cafbcdf5665dafada69a09243e897d0045b
treinY <invisiblehovercraft5@gmail.com>
2025-06-01 16:52:41 +0630

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Button ကို ရွေးပြီး click event လုပ်မယ်
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', () => {
+  console.log("Button clicked! Let's do something awesome 🚀");
+  alert("Thanks for clicking! You rock! 🤟");
+});
